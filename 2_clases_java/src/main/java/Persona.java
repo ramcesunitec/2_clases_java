d7f188a8f9874e0622f47b9210b2912f052a7f27
ramce <ramce@RAMCES-PC972212>
2020-02-06 14:16:59 -0600

    Comienzo

---
+++ 2_clases_java/src/main/java/Persona.java
@@ -0,0 +1,14 @@
+//Definicion de clase(plantilla)
+
+public class Persona {
+    //Atributos
+    String nombre;
+    String apellido;
+    
+    //Metodo de la clase
+    //Lo usaran los objetos de esta clase
+    public void desplegarNombre(){
+        System.out.println("Nombre: "+nombre);
+        System.out.println("Apellido: "+apellido);
+    }
+}
